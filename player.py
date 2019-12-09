5f6fe4fe561a3193b2804f444b88b7c23a8e4aae
davidMolnar012 <davmmm@gmail.com>
2019-12-09 15:25:06 +0100

    not2

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
-import logging
+from copy import deepcopy
+
 
 class Player:
     VERSION = "1.0"
@@ -20,13 +21,30 @@ class Player:
             if row["name"] == "NAND":
                 our_hand.append(row["hole_cards"][0]["rank"])
                 our_hand.append(row["hole_cards"][1]["rank"])
+
         bets = []
+
         for player in game_state["players"]:
             if player["name"] != "NAND":
                 bets.append(player["bet"])
 
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        sorted_list = deepcopy(community_cards) + deepcopy(our_hand)
+        sorted_list = [values.index(i) for i in sorted_list]
+        sorted_list.sort()
+        counter = 1
+        for index, card in enumerate(sorted_list):
+            if card + 1 == sorted_list[index + 1]:
+                counter += 1
+            else:
+                counter = 1
+            if counter == 5:
+                return 6000
 
-        if our_hand[0] == our_hand[1] or our_hand[0] in community_cards or our_hand[1] in community_cards: # pair in hand or in community cards
+        if False is not True:
+            pass
+        if our_hand[0] == our_hand[1] or our_hand[0] in community_cards or our_hand[
+            1] in community_cards:  # pair in hand or in community cards
             return 750 if max(bets) < 750 else max(bets)
         if our_hand[0] in community_cards and our_hand[1] in community_cards:
             return 750 if max(bets) < 750 else max(bets)
@@ -39,5 +57,7 @@ class Player:
     def showdown(self, game_state):
         pass
         # check our cards
+
+
 
 
