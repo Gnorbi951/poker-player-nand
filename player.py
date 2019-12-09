59e43cf2c4885c364adadd94bbe840a81f3116cb
davidMolnar012 <davmmm@gmail.com>
2019-12-09 10:54:24 +0100

    log

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+import csv
 
 class Player:
     VERSION = "1.0"
@@ -6,5 +7,9 @@ class Player:
         return 10
 
     def showdown(self, game_state):
+        with open('log.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile, delimiter=' ')
+            for row in game_state:
+                writer.writerow(row)
         pass
 
