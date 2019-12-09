44d30ea620a28a3ae5b3569ca2544665a053841e
davidMolnar012 <davmmm@gmail.com>
2019-12-09 11:51:30 +0100

    loging try

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-import sys
+import logging
 
 class Player:
     VERSION = "1.0"
@@ -24,7 +24,7 @@ class Player:
         return 250
 
     def showdown(self, game_state):
-        sys.stdout(game_state)
+        logging.warning(game_state)
         # check our cards
 
 
