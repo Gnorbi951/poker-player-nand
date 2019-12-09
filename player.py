8233c8cf71e8663df675bede0e214e3f9ddd574f
davidMolnar012 <davmmm@gmail.com>
2019-12-09 10:56:30 +0100

    increase bet

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class Player:
     VERSION = "1.0"
 
     def betRequest(self, game_state):
-        return 10
+        return 20
 
     def showdown(self, game_state):
         with open('log.csv', 'w') as csvfile:
