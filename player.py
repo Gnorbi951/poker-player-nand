d38bebd088d87a74f2b3e1ac4f23c7e70a197352
Gnorbi951 <gnorbi951@gmail.com>
2019-12-09 13:42:59 +0100

    Fold static blinds

--- player.py
+++ player.py
@@ -31,7 +31,7 @@ class Player:
         if our_hand[0] in community_cards and our_hand[1] in community_cards:
             return max(bets)
 
-        return 10
+        return 15
 
     def showdown(self, game_state):
         pass
